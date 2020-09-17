986800f5e85a7a292c7cfed1c9a196efcb10c23f
margareta75 <margareta.xx@gmail.com>
2020-09-17 19:52:16 +0200

    docs

---
+++ docs/precache-manifest.9a5c4f20e90c02950519c8f89287b490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16a2501914494e1acdca4a08346ab82",
+    "url": "/react-flickr-app/index.html"
+  },
+  {
+    "revision": "c5a33efbe0263e39de25",
+    "url": "/react-flickr-app/static/css/main.118fcc3d.chunk.css"
+  },
+  {
+    "revision": "12b6a233cd4255534bd9",
+    "url": "/react-flickr-app/static/js/2.90535927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flickr-app/static/js/2.90535927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a33efbe0263e39de25",
+    "url": "/react-flickr-app/static/js/main.835f47fb.chunk.js"
+  },
+  {
+    "revision": "1a4150d0d42badb197a9",
+    "url": "/react-flickr-app/static/js/runtime-main.e64589c8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd91af6b90a47be776bea7e1f2cdd193.js"
+  "/react-flickr-app/precache-manifest.9a5c4f20e90c02950519c8f89287b490.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-flickr-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
