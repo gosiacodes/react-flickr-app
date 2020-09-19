c95ea784ef27d3a38790bb31968de0ba8e6011b6
margareta75 <margareta.xx@gmail.com>
2020-09-19 21:25:06 +0200

    Testing docs

---
+++ docs/precache-manifest.19f7167ac49a589e5af2b03c4446c77f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8616e4320f42c06e6303ad9cfb8bbf7f",
+    "url": "/react-flickr-app/index.html"
+  },
+  {
+    "revision": "19e0750767efe4077499",
+    "url": "/react-flickr-app/static/css/main.fa341618.chunk.css"
+  },
+  {
+    "revision": "011b0fe836e258902b1b",
+    "url": "/react-flickr-app/static/js/2.31400313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flickr-app/static/js/2.31400313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e0750767efe4077499",
+    "url": "/react-flickr-app/static/js/main.e208bd0f.chunk.js"
+  },
+  {
+    "revision": "1a4150d0d42badb197a9",
+    "url": "/react-flickr-app/static/js/runtime-main.e64589c8.js"
+  }
+]);
